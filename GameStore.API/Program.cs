abf6d8def7d205df7769c84c8ab76eeb9670c203
Nico Auerswald <auersw.nico@gmail.com>
2026-05-19 16:05:07 +0200

    handle GET and PUT on non existing resources

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -16,8 +16,13 @@ List<GameDto> games =
 app.MapGet("/games", () => games);
 
 // GET /games/1
-app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
-   .WithName(GetGameEndpointName);
+app.MapGet("/games/{id}", (int id) =>
+{
+   var game = games.Find(game => game.Id == id); 
+
+   return game is null ? Results.NotFound() : Results.Ok(game);
+})
+.WithName(GetGameEndpointName);
 
 // POST /games
 app.MapPost("/games", (CreateGameDto newGame) =>
@@ -39,6 +44,11 @@ app.MapPost("/games", (CreateGameDto newGame) =>
 app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) => 
 {
     var index = games.FindIndex(game => game.Id == id);
+
+    if (index == -1)
+    {
+        return Results.NotFound();
+    }
 
     games[index] = new GameDto (
         id,
